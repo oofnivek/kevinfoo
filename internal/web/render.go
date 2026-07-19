613ad11d24b06b5c7ee20db0802c038318aec2ca
Kevin FOO <oofnivek@gmail.com>
2026-07-20 00:03:27 +0800

    fix icon on every row bug

--- internal/web/render.go
+++ internal/web/render.go
@@ -6,17 +6,28 @@ import (
 	"fmt"
 	"html/template"
 	"io"
+	"strings"
 )
 
 //go:embed all:templates
 var templatesFS embed.FS
+
+var funcMap = template.FuncMap{
+	// initial returns the uppercased first character of s, for row avatars.
+	"initial": func(s string) string {
+		for _, r := range s {
+			return strings.ToUpper(string(r))
+		}
+		return ""
+	},
+}
 
 type Renderer struct {
 	tmpl *template.Template
 }
 
 func NewRenderer() (*Renderer, error) {
-	tmpl, err := template.ParseFS(templatesFS, "templates/*.html", "templates/partials/*.html")
+	tmpl, err := template.New("").Funcs(funcMap).ParseFS(templatesFS, "templates/*.html", "templates/partials/*.html")
 	if err != nil {
 		return nil, fmt.Errorf("parse templates: %w", err)
 	}
